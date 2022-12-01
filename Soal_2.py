e356e0b3864c1eb2b95db8d297984896102dcd79
Fransiskadani <117888728+Fransiskadani@users.noreply.github.com>
2022-12-01 13:30:38 +0700

    Add files via upload

---
+++ Soal_2.py
@@ -0,0 +1,9 @@
+def funPalindrome () :
+    kata = (input("Masukkan kata :"))
+    if kata == 'katak' :
+        print ("yes")
+        print ("Jika di balik :",kata)
+    else :
+        print ("no")
+        print ("Jika di balik :",kata)
+funPalindrome()
