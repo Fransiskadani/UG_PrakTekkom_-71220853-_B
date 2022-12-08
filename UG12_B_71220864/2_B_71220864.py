e4c97f1153a409bc41aef784fb44c57d8a653a95
Fransiskadani <117888728+Fransiskadani@users.noreply.github.com>
2022-12-08 12:48:21 +0700

    Add files via upload

---
+++ UG12_B_71220864/2_B_71220864.py
@@ -0,0 +1,5 @@
+print("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
+a = input("Masukkan operator matematika yang ingin Anda hitung: ")
+b = input("Mau penjumlahan berapa: ")
+c = input(" Print berapa banyak")
+
