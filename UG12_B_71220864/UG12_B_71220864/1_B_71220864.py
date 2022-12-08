43cc988a0e235a6fce7fd93ee75a7e355f2d364f
Fransiskadani <117888728+Fransiskadani@users.noreply.github.com>
2022-12-08 12:50:59 +0700

    Add files via upload

---
+++ UG12_B_71220864/UG12_B_71220864/1_B_71220864.py
@@ -0,0 +1,6 @@
+print("SELAMAT DATANG DI PROGRAM PEMBUAT PIRAMIDA BERULANG")
+Q = int(input("Masukkan Angka : "))
+print (" "*(Q-1),("*"))
+for W in range ((Q-1),0,-1):
+    print(' '*(W-1),"**")
+print("**"*Q)
